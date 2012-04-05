a42eddadfa76fc5c9f2222d83c84324305bfaee6
Kurmaev Serg <bigmozgizerg@gmail.com>
2012-04-05 11:23:46 +0700

    #7 add category model

--- event/models.py
+++ event/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+class Category(models.Model):
+    shortname = models.CharField(max_length=10)
+    description = models.CharField(max_length=200)
+
+
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    date = models.DateField()
-    category = models.CharField(max_length=10)
+    date = models.DateField()   
+    category = models.ManyToManyField(Category)
     text = models.CharField(max_length=1024)
     picture = models.ImageField(upload_to='/pic')
