71e54ff77a4f976b26f309f8427caa444761ee4d
Serg Kurmaev <bigmozgizerg@gmail.com>
2012-05-08 22:41:12 +0700

    Download images

--- eventcrawler/pipelines.py
+++ eventcrawler/pipelines.py
@@ -4,20 +4,21 @@ import datetime
 from event.models import Event,Category
 from django.db.utils import IntegrityError
 from scrapy.exceptions import DropItem
-
+from hashlib import sha1
 
 class EventcrawlerPipeline(object):
     def __init__(self):
         self.category = Category.objects.get(shortname='gallery')
         
     def process_item(self, item, spider):
+        pic_name = "%s%s.jpg" % ('images/full/', 
+                                sha1(item['image_urls'][0]).hexdigest())
+
 
         event = Event(text=item['text'],name=item['name'],
                         date_start=item['date_start'], date_end=item['date_end'],
                         category=self.category, added_by='admin',
-                        place=item['place_time'])
-#                        picture='/media/images/'+item['image'][0]['path'])
-        event.save()
+                        place=item['place_time'], picture=pic_name)
         try:
             event.save()
         except IntegrityError:
