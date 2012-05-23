d4e01b4e314ad936cda04e325add5c7b2c2df5c1
Kurmaev Serg <bigmozgizerg@gmail.com>
2012-05-23 12:41:22 +0700

    del link to create/edit profile, add dropdown navbar

--- firstsite/views.py
+++ firstsite/views.py
@@ -4,6 +4,9 @@ import datetime
 
 def randpage(request):
     today = datetime.date.today()
+### Delete after show!!!!
+    from django.core.cache import cache
+    cache.clear()
     list_events = Event.objects.raw('select * from event_event where date_end>=\
 "%s" order by rand() LIMIT 5'% (today.isoformat()))
 
