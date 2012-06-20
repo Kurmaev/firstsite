1b82ce792937e4dbd62d90d64471945d83f04ebf
Kurmaev Serg <bigmozgizerg@gmail.com>
2012-06-20 10:43:41 +0700

    added clear cache task

---
+++ event/tasks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from celery.decorators import task, periodic_task
+
+@periodic_task(run_every=crontab(minute=0, hour=13)#подстроено под сервер, 00 Омск
+def clear_cache():
+    from django.core.cache import cache
+    cache.clear()
+
+#проверить на сервере
+#@periodic_task(run_every=crontab(minute=0, hour=15)#02.00 Omsk
+#def run_scrapy():
+#    import subprocess
+#    MY_PATH = os.path.dirname(__file__)
+#    subprocess.call([os.path.join(MY_PATH,'run_scrapy.sh'),])
